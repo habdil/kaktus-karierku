eba3bcb30c866d2a7643bef023a8552ef099b89e
habdil <habdiliqrawardana@gmail.com>
2024-10-27 18:56:55 +0700

    update brow

--- app/(client)/dashboard/consultation/[mentorId]/page.tsx
+++ app/(client)/dashboard/consultation/[mentorId]/page.tsx
@@ -1,1 +1,15 @@
+import { FeatureMaintenance } from "@/components/shared/FeatureMaintanace";
+
 // app\(client)\dashboard\consultation\[mentorId]\page.tsx
+const ConsultationClientId = () => {
+    return ( 
+        <div className="flex justify-center items-center h-screen">
+        <FeatureMaintenance
+            title="Tahap Pengambangan"
+            description="Kami sedang mengembangkan fitur ini untuk memperbaiki dan meningkatkan kualitas layanan kami."
+        />
+    </div>
+     );
+}
+ 
+export default ConsultationClientId;
