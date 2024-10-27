9e43e6a28ada0d8a8def2445979c0a4a7225b34b
habdil <habdiliqrawardana@gmail.com>
2024-10-27 18:55:03 +0700

    update maintanance

---
+++ app/(admin)/dashboard-admin/mentors/[mentorId]/page.tsx
@@ -0,0 +1,16 @@
+import { FeatureMaintenance } from "@/components/shared/FeatureMaintanace";
+
+const MentorAdminIdPage = () => {
+    return ( 
+        <div>
+            <div className="flex justify-center items-center h-screen">
+        <FeatureMaintenance
+            title="Tahap Pengambangan"
+            description="Kami sedang mengembangkan fitur ini untuk memperbaiki dan meningkatkan kualitas layanan kami."
+        />
+    </div>
+        </div>
+     );
+}
+ 
+export default MentorAdminIdPage;
